74ec3d6a6f0ea8dcd0cfa915bd5547f23a150e9b
Fillipe de Souza Silva <fssilva@fiecalunos.lan>
2022-03-07 20:31:12 -0300

    Adicionando formulario para alunos

--- src/AlunoForm.js
+++ src/AlunoForm.js
@@ -1,14 +1,38 @@
-import React, {useState} from 'react'
+import React, {useState, useReducer} from 'react'
 
+const formReducer = (state, action) => {
+  switch(action.type){
+    case 'ATUALIZA':
+      return {
+        ...state,
+        [action.name]: action.value
+      }
+    default:
+      return state;
+  }
+}
 const AlunoForm = () => {
-    let initialState = {}
-    const [form, setForm] = useState(initialState);
-    
-
+    const initialState = { rm: "", nome: "",  curso: "",  profileImage: "" }
+    const [formState, dispatch] = useReducer(formReducer, initialState);
     const handleChange = (e) => {
-        //console.log(e.target.name + " " + e.target.value)
-        setForm({ [e.target.name] : e.target.value })
-        console.log(form)
+        dispatch({
+          type: 'ATUALIZA',
+          name: e.target.name,
+          value: e.target.value
+        })
+        
+    }
+
+    const submitForm = (e) => {
+      const url = "http://localhost:8080/alunos"
+      e.preventDefault();
+      console.log(formState)
+      fetch(url, {
+        method: 'POST',
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(formState)
+      }).then(response => response.json())
+      .then(data => alert("Dados enviados com sucesso"));
     }
 
     return (
@@ -17,28 +41,32 @@ const AlunoForm = () => {
                 <div className="form-group">
                   <label for="rm">RM</label>
                   <input type="number" onChange={handleChange}
-                    className="form-control" name="rm" id="rm" aria-describedby="helpId" placeholder="" value="" />
+                    className="form-control" name="rm" id="rm" aria-describedby="helpId" placeholder=""
+                     value={formState.rm} />
                   <small id="helpId" className="form-text text-muted">Coloque o RM do aluno</small>
                 </div>
                 <div className="form-group">
                   <label for="nome">Nome</label>
                   <input type="text" onChange={handleChange}
-                    className="form-control" name="nome" id="nome" aria-describedby="helpId" placeholder="" />
+                    className="form-control" name="nome" id="nome" aria-describedby="helpId" placeholder=""
+                     value={formState.nome} />
                   <small id="helpId" className="form-text text-muted">Coloque o Nome do aluno</small>
                 </div>
                 <div className="form-group">
                   <label for="curso">Curso</label>
                   <input type="text" onChange={handleChange}
-                    className="form-control" name="curso" id="curso" aria-describedby="helpId" placeholder="" />
+                    className="form-control" name="curso" id="curso" aria-describedby="helpId" placeholder="" 
+                    value={formState.curso} />
                   <small id="helpId" className="form-text text-muted">Coloque o Curso do aluno</small>
                 </div>
                 <div className="form-group">
                   <label for="profileImage">Imagem</label>
                   <input type="text" onChange={handleChange}
-                    className="form-control" name="profileImage" id="profileImage" aria-describedby="helpId" placeholder="" />
+                    className="form-control" name="profileImage" id="profileImage" aria-describedby="helpId" placeholder=""
+                     value={formState.profileImage} />
                   <small id="helpId" className="form-text text-muted">Coloque a Imagem do aluno</small>
                 </div>
-               <button type="submit" className="btn btn-primary">Submit</button> 
+               <button type="submit" onClick={submitForm} className="btn btn-primary">Submit</button> 
             </form>
         </div>
     )
